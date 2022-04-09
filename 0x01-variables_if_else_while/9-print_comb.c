1173a8b36f7e6c3a517cdc11b885ce0a00d1b893
Jemima Adjanor <49441916+jejeadjanor@users.noreply.github.com>
2022-04-09 04:02:39 +0000

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,22 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	for (; n <= 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+			break;
+		putchar(',');
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
