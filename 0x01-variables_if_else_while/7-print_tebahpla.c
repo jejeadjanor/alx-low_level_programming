a815e672b17a1de9c488541bd90b9eb30eea4399
Jemima Adjanor <49441916+jejeadjanor@users.noreply.github.com>
2022-04-09 04:00:00 +0000

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	for (; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
