6153a7dde8e787ca66725d7765a455b2333b13a2
Jemima Adjanor <49441916+jejeadjanor@users.noreply.github.com>
2022-04-09 03:54:40 +0000

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a', y = 'A';
+
+	for (; x <= 'z'; x++)
+		putchar(x);
+	for (; y <= 'Z'; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+}
