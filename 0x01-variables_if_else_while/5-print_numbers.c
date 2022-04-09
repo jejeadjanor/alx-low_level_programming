088f3fd8dab4c4fe8a374f27ab80ddbd3b125f6b
Jemima Adjanor <49441916+jejeadjanor@users.noreply.github.com>
2022-04-09 03:57:10 +0000

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d = 0;
+
+	for (; d != 10; d++)
+		printf("%d", d);
+	printf("\n");
+	return (0);
+}
