e3e185b5f87aa8039d6ab605c51c2aabbbae73e3
Jemima Adjanor <49441916+jejeadjanor@users.noreply.github.com>
2022-04-13 21:53:34 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * main - Print "Jemima" followed by a new line.
+ * main - Print "Holberton" followed by a new line.
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
 int main(void)
 {
 int c = 0;
-char jemima[10] = "_putchar\n";
+char holberton[10] = "_putchar\n";
 
 while (c < 9)
 {
-_putchar(jemima[c]);
+_putchar(holberton[c]);
 c++;
 }
 return (0);
